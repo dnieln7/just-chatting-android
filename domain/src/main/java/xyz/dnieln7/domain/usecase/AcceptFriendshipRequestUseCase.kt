949a4f68394ff95b69c5a7a602b6ed9f421003a1
Daniel <dnieln7@gmail.com>
2023-11-01 20:10:25 -0600

    Added eitherCatch to safely get current user

--- domain/src/main/java/xyz/dnieln7/domain/usecase/AcceptFriendshipRequestUseCase.kt
+++ domain/src/main/java/xyz/dnieln7/domain/usecase/AcceptFriendshipRequestUseCase.kt
@@ -1,16 +1,25 @@
 package xyz.dnieln7.domain.usecase
 
 import arrow.core.Either
+import arrow.core.raise.either
+import kotlinx.coroutines.flow.first
+import xyz.dnieln7.domain.extension.eitherCatch
+import xyz.dnieln7.domain.preferences.DataStorePreferences
 import xyz.dnieln7.domain.repository.FriendshipRepository
 import javax.inject.Inject
 
 class AcceptFriendshipRequestUseCase @Inject constructor(
+    private val dataStorePreferences: DataStorePreferences,
     private val friendshipRepository: FriendshipRepository,
     private val getErrorFromThrowableUseCase: GetErrorFromThrowableUseCase
 ) {
 
-    suspend operator fun invoke(userID: String, friendID: String): Either<String, Unit> {
-        return friendshipRepository.acceptFriendshipRequest(userID, friendID).mapLeft {
+    suspend operator fun invoke(friendID: String): Either<String, Unit> {
+        return either {
+            val user = eitherCatch { dataStorePreferences.getUser().first()!! }
+
+            friendshipRepository.acceptFriendshipRequest(user.id, friendID).bind()
+        }.mapLeft {
             getErrorFromThrowableUseCase(it)
         }
     }
