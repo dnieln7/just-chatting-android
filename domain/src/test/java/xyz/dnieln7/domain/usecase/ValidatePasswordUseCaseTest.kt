f224a3e95aa63d7fadccd5a7870ddf6c57399714
dnieln7 <dnieln7@gmail.com>
2024-07-03 22:28:37 -0600

    updated test to use ValidationStatus

--- domain/src/test/java/xyz/dnieln7/domain/usecase/ValidatePasswordUseCaseTest.kt
+++ domain/src/test/java/xyz/dnieln7/domain/usecase/ValidatePasswordUseCaseTest.kt
@@ -3,6 +3,7 @@ package xyz.dnieln7.domain.usecase
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.Before
 import org.junit.Test
+import xyz.dnieln7.domain.validation.ValidationStatus
 
 class ValidatePasswordUseCaseTest {
 
@@ -14,29 +15,29 @@ class ValidatePasswordUseCaseTest {
     }
 
     @Test
-    fun `GIVEN the happy path WHEN invoke THEN return Unit`() {
+    fun `GIVEN the happy path WHEN invoke THEN return Valid`() {
         val password = "123456789012"
 
-        val result = useCase(password).getOrNull()
+        val result = useCase(password)
 
-        result shouldBeEqualTo Unit
+        result shouldBeEqualTo ValidationStatus.Password.Valid
     }
 
     @Test
-    fun `GIVEN an empty password WHEN invoke THEN return PasswordsValidation_EMPTY`() {
+    fun `GIVEN an empty password WHEN invoke THEN return EMPTY`() {
         val password = ""
 
-        val result = useCase(password).swap().getOrNull()
+        val result = useCase(password)
 
-        result shouldBeEqualTo PasswordValidationError.EMPTY
+        result shouldBeEqualTo ValidationStatus.Password.Invalid.EMPTY
     }
 
     @Test
-    fun `GIVEN a password with less than MINIMUM_PASSWORD_LENGTH characters WHEN invoke THEN return PasswordsValidation_TOO_SHORT`() {
+    fun `GIVEN a password with less than MINIMUM_PASSWORD_LENGTH characters WHEN invoke THEN return TOO_SHORT`() {
         val password = "1234567"
 
-        val result = useCase(password).swap().getOrNull()
+        val result = useCase(password)
 
-        result shouldBeEqualTo PasswordValidationError.TOO_SHORT
+        result shouldBeEqualTo ValidationStatus.Password.Invalid.TOO_SHORT
     }
 }
